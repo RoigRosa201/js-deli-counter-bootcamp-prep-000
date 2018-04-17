7e62d6199d8cf45afcb04e9381974baa643bf7e0
Roigrosa <roigrosa88@gmail.com>
2018-04-17 19:38:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber (katzDeliLine, name){
 var position = katzDeliLine.length+1
   katzDeliLine.push(name)
-return "Welcome, " + name +". You are number "+ position +" in line."
+  return "Welcome, " + name +". You are number "+ position +" in line."
 
 }
 
@@ -10,4 +10,21 @@ var name;
 
 if(deliLine.length == 0){
   return "There is nobody waiting to be served!"
+}
+
+  name = deliLine.shift()
+  return "Currently serving "+ name + "."
+}
+
+function currentLine (katzDeliLine){
+  if(katzDeliLine.length == 0){
+    return "The line is currently empty."
+  }
+
+  var myString = `The line is currently: 1. ${katzDeliLine[0]}`
+
+  for(let i = 1; i<katzDeliLine.length;i++){
+    myString += `, ${i+1}. ${katzDeliLine[i]}`
+}
+  return myString
 }
